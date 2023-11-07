e2651c2a764e24376b93217748d46bf7fd478b10
Ciprian Mandache <ciprian@51k.eu>
2023-11-07 23:39:22 +0200

    more sane defaults

--- log.go
+++ log.go
@@ -14,8 +14,8 @@ const (
 
 const (
 	defaultReportCaller = true
-	defaultLevel        = levelDebug
-	defaultFormat       = formatJSON
+	defaultLevel        = levelWarn
+	defaultFormat       = formatText
 )
 
 type config struct {
